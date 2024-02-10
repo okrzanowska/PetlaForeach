67ab16517351417c946d30a3b040a96c524e4d5b
Oliwia <106619574+okrzanowska@users.noreply.github.com>
2024-02-10 13:20:31 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace PetlaForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> pets = new List<string> { "cat", "dog", "parrot", "guinea pig" };
+
+            foreach (string pet in pets)
+            {
+                Console.WriteLine(pet);
+
+                if (pet == "dog")
+                {
+                    Console.WriteLine("End");
+
+                    break;
+                    //continue;
+                }
+            }
+        }
+    }
+}
